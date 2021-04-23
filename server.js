4e713d04c3582f4b327c95dfd3030d6323213705
Odubolaoluwatimilehin <odubolaoluwatimilehin@gmail.com>
2021-04-23 19:17:08 +0100

    chore: k82 deployment file fixed

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
+app.listen(8082, () => {
+  console.log("Server running on port 8082");
 });
 
 app.get("/health", (req, res, next) => {
